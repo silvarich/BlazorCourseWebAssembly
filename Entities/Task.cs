0b5f703f9b15d51e9fcf990ae382c8dc1555a9b4
silvarich <oliveirarichard49@gmail.com>
2022-01-22 20:50:54 -0300

    1/22

---
+++ Entities/Task.cs
@@ -0,0 +1,11 @@
+﻿namespace BlazorCourseWebAssembly.Entities
+{
+    public class Task
+    {
+        public Guid ID { get; set; }
+        public string Description { get; set; }
+        public bool Done { get; set; }
+        public DateTime CreationDate { get; set; }
+
+    }
+}
